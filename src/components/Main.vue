6ad169043d57e221134e8d38a299230b58f69548
Junwoong <chawnsnd@gmail.com>
2018-09-20 15:41:10 +0900

    지도, 검색바 삽입

---
+++ src/components/Main.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="main">
+        <search-bar />
+        <div id="map_div" class="map">
+        </div>        
+    </div>
+</template>
+
+<script>
+import SearchBar from './SearchBar'
+
+export default {
+    components: {
+        'search-bar': SearchBar
+    },
+    methods: {
+        initTmap(){
+            var map = new Tmap.Map({
+                div:'map_div',
+                width : window.innerWidth,
+                height : window.innerHeight,
+            });
+            map.setCenter(new Tmap.LonLat("126.986072", "37.570028").transform("EPSG:4326", "EPSG:3857"), 15);
+        } 
+    },
+    mounted(){
+        this.initTmap();
+        console.log(window.innerHeight)
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.main{
+    position: relative;
+    .search_bar{
+        position: absolute;
+        margin: 10px 0 0 10px;
+    }
+    .map{
+        position: absolute;
+        z-index: -100;
+    }
+}
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="search_bar">
+        <input type="text" class="input" placeholder="장소 검색"/>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.search_bar{
+    background-color: white;
+    width: 350px;
+    height: 50px;
+    border: 2px solid #eaeaea;
+    line-height: 50px;
+}
+.input{
+    border: none;
+    outline: none;
+    width: 90%;
+    height: 90%;
+}
+</style>

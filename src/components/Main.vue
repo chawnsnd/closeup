6b81fc42877770f034e8cf7bfe5673cd00193c80
bkh_linux_desktop <bkh751@gmail.com>
2018-11-10 17:08:21 +0900

    불필요한 코드 제거

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -33,12 +33,6 @@ export default {
   },
   methods: {},
   mounted() {
-    var name = "junwoongCha";
-    // var exampleSocket = new WebSocket("ws://ec2-13-59-71-223.us-east-2.compute.amazonaws.com:49152");
-    var exampleSocket = new WebSocket("ws://ec2-13-59-71-223.us-east-2.compute.amazonaws.com:49152/");
-    exampleSocket.onopen = function(event) {
-      exampleSocket.send(name);
-    };
   }
 };
 </script>

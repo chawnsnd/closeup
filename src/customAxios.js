a5c00bc030c4561f9b68530a34b497b02b164580
Junwoong <chawnsnd@gmail.com>
2018-11-21 16:42:41 +0900

    api 주소 수정

--- src/customAxios.js
+++ src/customAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
 //   baseURL: "http://ec2-13-125-249-233.ap-northeast-2.compute.amazonaws.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:5000/api",
   timeout: 3000,
   withCredentials: true,
   headers: {
